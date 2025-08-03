fade6c875a0e23e03e8c68085a248eb12039c5bc
jklauzar <joseph@klauzar.com>
2025-08-03 11:58:01 -0400

    Task 7 c

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,13 +8,20 @@ def emotion_detector(text_to_analyze):
     input_json = json.dumps(input_json)
 
     response = requests.post(url, headers = headers, data = input_json)
-
-    emotion_input = json.loads(response.text)["emotionPredictions"][0]["emotion"]
-    anger_score = emotion_input["anger"]
-    disgust_score = emotion_input["disgust"]
-    fear_score = emotion_input["fear"]
-    joy_score = emotion_input["joy"]
-    sadness_score = emotion_input["sadness"]
+    status_code = response.status_code
+    if status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+    else:
+        emotion_input = json.loads(response.text)["emotionPredictions"][0]["emotion"]
+        anger_score = emotion_input["anger"]
+        disgust_score = emotion_input["disgust"]
+        fear_score = emotion_input["fear"]
+        joy_score = emotion_input["joy"]
+        sadness_score = emotion_input["sadness"]
     
 
     emotion_output = {
@@ -24,8 +31,10 @@ def emotion_detector(text_to_analyze):
     'joy': joy_score,
     'sadness': sadness_score,
     }
-
-    dominant_emotion = max(emotion_output, key = emotion_output.get)
+    if response.status_code == 400:
+        dominant_emotion = None
+    else: 
+        dominant_emotion = max(emotion_output, key = emotion_output.get)
     emotion_output["dominant_emotion"] = dominant_emotion
 
 
--- server.py
+++ server.py
@@ -12,5 +12,7 @@ def emotionDetection():
     text = request.args.get("textToAnalyze")
     print(text)
     output = emotion_detector(text)
+    if output['dominant_emotion'] == None:
+        return "Invalid text, please try again"
     output_text = f"For the given statement, the system response is 'anger': {output['anger']}, 'disgust': {output['disgust']}, 'fear': {output['fear']}, 'joy': {output['joy']} and 'sadness': {output['sadness']}. The dominant emotion is {output['dominant_emotion']}."
     return output_text
